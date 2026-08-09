df176293e3637a834e3cbd3312a4ae9c3cc631a7
Aleksandr Slokov <thanhzeus2016@gmail.com>
2026-08-09 19:06:57 +0700

    feat: add Kuberina whitepaper documentation and supporting PDF asset

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,19 +6,19 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: "Aleksandr Slokov's whitepapers",
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/AlexanderSlokov/whitepapers' }],
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Whitepapers',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Kuberina Paper', slug: 'naughtian_kuberina/paper' },
 					],
 				},
 				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
+					label: 'References',
+					items: [{ autogenerate: { directory: 'references' } }],
 				},
 			],
 		}),
